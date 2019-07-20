3dfdbc6c5d3a171589993ca0cb26c0d76c4e8638
Angel Alanis <a4alaniz@gmail.com>
2019-07-20 20:36:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,15 +6,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, obj, {[key]: value})
   var obj = {prop: 1}
 }
-updateObjectWithKeyAndValue(obj, 'prop2', 2)
-obj
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  return ojbect
-}
-var obj = {prop: 1}
-destructivelyUpdateObjectWithKeyAndValue(obj, 'prop 2,' 2)
-obj
 
 
 function deleteFromObjectByKey(object, key) {

757fbb232f601d3b7bf794bfcc191fdc5995cff4
Angel Alanis <a4alaniz@gmail.com>
2019-07-20 20:20:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 function objects() {
   object[recipes] = objects
 }
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, obj, {[key]: value})
+  var obj = {prop: 1}
+}
+updateObjectWithKeyAndValue(obj, 'prop2', 2)
+obj
